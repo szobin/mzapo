c7a47c986bc23d50bc85fd5cb8dc9a201f21f298
Sergey Zobin <zobin.sergey@gmail.com>
2019-05-09 13:54:06 +0300

    upd26

--- mzapo_func.h
+++ mzapo_func.h
@@ -4,18 +4,6 @@
 
 #include <stdint.h>
 
-const uint16_t clWhite = 0x0000;
-const uint16_t clBlack = 0xffff;
-const uint16_t clGray  = 0x8410;
-
-const uint16_t clBlue  = 0x001f;
-const uint16_t clGreen = 0x07e0;
-const uint16_t clRed   = 0xf800;
-
-const uint16_t clSky     = 0x0010;
-const uint16_t clLtGreen = 0x0400;
-const uint16_t clLtRed   = 0x8000;
-
 typedef struct {
     uint16_t color_led_1;
     uint16_t color_led_2;
